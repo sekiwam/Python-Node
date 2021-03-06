ec03e23bde6572b00afa5c6cf33d913fd686fec3
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-07 06:58:49 +0900

    clean in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-git submodule update -i
+git submodule update --remote -i
 
 NODE_DIR="nodejs"
 VERSION="1.0.0"
@@ -11,4 +10,6 @@ cp python_node.cc $NODE_DIR/src/
 cp python_node.h $NODE_DIR/src/
 
 cd $NODE_DIR;
+git reset --hard 
+git clean -f -d
 git apply ../diff.patch
