93dbc4b5e5145deae3d7ba3003c86e9c86b5d46f
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-07 07:01:43 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-
-git submodule update --remote -i
-
 NODE_DIR="nodejs"
 VERSION="1.0.0"
 echo $VERSION
+
+cd $NODE_DIR;
+git reset --hard 
+git clean -f -d
+cd -;
+git submodule update --remote -i
+
 cp python.js $NODE_DIR/lib/
 cp python_node.cc $NODE_DIR/src/
 cp python_node.h $NODE_DIR/src/
 
 cd $NODE_DIR;
-git reset --hard 
-git clean -f -d
 git apply ../diff.patch
