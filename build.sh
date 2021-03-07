377a510ce5b95744dd1975a3b43526c9c6417bd9
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-08 00:01:48 +0900

    win

--- build.sh
+++ build.sh
@@ -4,6 +4,18 @@ VERSION="1.0.0"
 NODE_VERSION=$1
 PATCH_FILE=diff_$1.patch
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+
+
+
 if [[ ! -f $PATCH_FILE ]] ; then
     echo "File $PATCH_FILE is not there, aborting. [v15, v14]"
     exit
@@ -32,6 +44,9 @@ git apply ../$PATCH_FILE
 cd ..
 
 
+# ----------------------------------------------------
+#                   Build Python 
+# ----------------------------------------------------
 python_url="https://www.python.org/ftp/python/3.8.8/Python-3.8.8.tgz"
 curl -O $python_url
 ls -al
@@ -39,4 +54,18 @@ tar zxvf Python-3.8.8.tgz
 cd Python-3.8.8
 ls -al
 cd PCbuild
-#start get_externals.bat
+
+
+if  [ "$2" != "--github_actions" ] ; then
+    if [ "$machine" = "MinGw" ] ; then
+        cd ..
+        cd ..
+        dir
+        start build_python.bat
+        #call get_externals.bat
+        #call build.bat
+        exit
+    fi 
+fi
+
+echo "3000"
---
+++ build_python.bat
@@ -0,0 +1,13 @@
+cd Python-3.8.8/PCbuild
+call get_externals.bat
+call build.bat
+
+
+
+
+
+
+
+
+
+
