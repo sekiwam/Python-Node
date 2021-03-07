989bc7d1c526f2ada821a1f45a9277caa19d8f2d
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-08 05:06:47 +0900

    try vcbuild on win2

--- build_python.bat
+++ build_python.bat
@@ -6,13 +6,20 @@ dir out/Python-%1/Include
 
 cd out/Python-%1/PCbuild
 call get_externals.bat
+errorLevel0=%errorlevel%
 call build.bat -p x64
+errorLevel1=%errorlevel%
 
 dir amd64
 
 cd ../../..
 cd nodejs
 call vcbuild.bat
+errorLevel2=%errorlevel%
+
+echo %errorLevel0%
+echo %errorLevel1%
+echo %errorLevel2%
 
 
 
