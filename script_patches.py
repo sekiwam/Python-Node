c8bc9213e865b0f4dfc0ca5b9d6d997d27367a71
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-15 05:42:07 +0900

    chane apply 4

--- script_patches.py
+++ script_patches.py
@@ -6,43 +6,19 @@ import platform
 
 platform_system = platform.system()
 
-# +      'lib/python.js',
-#        'lib/cluster.js',
-#        'lib/diagnostics_channel.js',
-#        'lib/dgram.js',
-# @@ -679,6 +680,8 @@
-#          'src/udp_wrap.cc',
-#          'src/util.cc',
-#          'src/uv.cc',
-# +            'src/python_node.cc',
-# +            'src/python_node.h',
 
 def put_libraries(libraries):
     print(libraries)
-    # with open('nodejs/node.gypi', 'r') as file:
-    #     filedata = file.read()
-
-    #     if not 'libpython3' in filedata:
-    #         # Replace the target string
-    #         filedata = filedata.replace('\'-lrt\'', '\'-lrt\'], \'libraries\': [\'libpython3.8.so\'')
-
-    #         # Write the file out again
-    #         with open('nodejs/node.gypi', 'w') as file:
-    #             file.write(filedata)
-
-    filedata = None
-    with open('nodejs/node.gyp', 'r') as file:
+    with open('nodejs/node.gypi', 'r') as file:
         filedata = file.read()
 
-    if filedata and  (not 'libpython' in filedata):
-        # Replace the target string
-        filedata = filedata.replace("'src/node_main.cc'", "'src/node_main.cc','src/python_node.cc','src/python_node.cc'], 'libraries': ['libpython3.8.so'")
+        if not 'libpython3' in filedata:
+            # Replace the target string
+            filedata = filedata.replace('\'-lrt\'', '\'-lrt\'], \'libraries\': [\'libpython3.8.so\'')
 
-        filedata = filedata.replace("'lib/crypto.js'", "'lib/crypto.js','lib/python.js'")
-
-        # Write the file out again
-        with open('nodejs/node.gyp', 'w') as file:
-            file.write(filedata)
+            # Write the file out again
+            with open('nodejs/node.gypi', 'w') as file:
+                file.write(filedata)
 
 
 print("args = " + str(sys.argv))
