e18f15332e9a48a2d47c3e6f7169015235837683
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-15 11:37:25 +0900

    Adds rpath to cctest 4

--- script_patches.py
+++ script_patches.py
@@ -30,10 +30,6 @@ def put_libraries(libraries):
             # Replace the target string
             filedata = filedata.replace("'target_name': 'node_mksnapshot'", "'target_name': 'node_mksnapshot', 'ldflags': [ \"-Wl,-rpath='$$ORIGIN/./'\"]")
 
-            # Write the file out again
-            with open('nodejs/node.gyp', 'w') as file:
-                file.write(filedata)
-
             # Replace the target string
             filedata = filedata.replace("'target_name': 'cctest'", "'target_name': 'cctest', 'ldflags': [ \"-Wl,-rpath='$$ORIGIN/./'\"]")
 
