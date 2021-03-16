d2c137b27fd8143058c986e490135817e7a91c36
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-17 02:28:34 +0900

    windows test 2

--- build_python.bat
+++ build_python.bat
@@ -9,6 +9,11 @@ call get_externals.bat
 call build.bat -p x64
 
 dir amd64
+mkdir ../../../nodejs/out
+mkdir ../../../nodejs/out/Release
+mkdir ../../../nodejs/out/Release/lib
+copy amd64\python38.lib ../../../nodejs/out/Release/
+copy amd64\python38.lib ../../../nodejs/out/Release/lib/
 
 cd ../../..
 cd nodejs
