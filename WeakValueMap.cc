5af9480d27d0de42f776313b323fb5e590d62adb
sekiwam <9922237+sekiwam@users.noreply.github.com>
2021-03-20 16:24:38 +0900

    improve WeakValueMap

--- WeakValueMap.cc
+++ WeakValueMap.cc
@@ -5,126 +5,132 @@
 
 #include "WeakValueMap.h"
 
-namespace python_node {
-	struct WeakValue;
+namespace python_node
+{
+    struct WeakValue;
 
+    struct FinalizationCbData
+    {
+        InstanceId_t weakValueMapInstanceId;
+        MapKeyPair *pair;
 
-	struct FinalizationCbData {
-		InstanceId_t weakValueMapInstanceId;
-		MapKeyPair* pair;
-
-		FinalizationCbData(InstanceId_t weakValueMapInstanceId, MapKeyPair* pair)
-			: weakValueMapInstanceId(weakValueMapInstanceId), pair(pair) {
-		}
-		~FinalizationCbData() {}
-	};
-
-
-	struct WeakValue {
-		MapKeyPair* pair = nullptr;
-		v8::UniquePersistent<v8::Value> value;
-		WeakValue(
-            std::unordered_map<std::string, WeakValue>* m,
-            std::string s,
-            v8::UniquePersistent<v8::Value>&& v
-        )
-        : value(std::move(v))
+        FinalizationCbData(InstanceId_t weakValueMapInstanceId, MapKeyPair *pair)
+            : weakValueMapInstanceId(weakValueMapInstanceId), pair(pair)
         {
-			this->pair = new MapKeyPair(m, s);
-		}
-		
-        ~WeakValue() {
-			if (this->pair != nullptr) {
-				delete this->pair;
-				this->pair = nullptr;
-			}
-		}
-		WeakValue() { }
-		WeakValue(WeakValue&& o) {
-			std::swap(this->pair, o.pair);
-			this->value = std::move(o.value);
-		}
-	};
+        }
+        ~FinalizationCbData() {}
+    };
 
+    struct WeakValue
+    {
+        MapKeyPair *pair = nullptr;
+        v8::UniquePersistent<v8::Value> value;
+        WeakValue(
+            std::unordered_map<std::string, WeakValue> *m,
+            std::string s,
+            v8::UniquePersistent<v8::Value> &&v)
+            : value(std::move(v))
+        {
+            this->pair = new MapKeyPair(m, s);
+        }
 
+        ~WeakValue()
+        {
+            if (this->pair != nullptr)
+            {
+                delete this->pair;
+                this->pair = nullptr;
+            }
+        }
+        WeakValue() {}
+        WeakValue(WeakValue &&o)
+        {
+            std::swap(this->pair, o.pair);
+            this->value = std::move(o.value);
+        }
+    };
 
-	WeakValueMap::WeakValueMap(
-		int(*test)(const std::string &key)
-	) {
-		this->map = new std::unordered_map<std::string, WeakValue>;
-		this->id = WeakValueMap::instanceId++;
-		WeakValueMap::instanceMap->insert(std::make_pair(this->id, test));
-	}
+    WeakValueMap::WeakValueMap(
+        int (*test)(const std::string &key))
+    {
+        this->map = new std::unordered_map<std::string, WeakValue>;
+        this->id = WeakValueMap::instanceId++;
+        WeakValueMap::instanceMap->insert(std::make_pair(this->id, test));
+    }
 
-	WeakValueMap::~WeakValueMap() {
-		delete this->map;
-		this->map = nullptr;
-		WeakValueMap::instanceMap->erase(this->id);
-	}
+    WeakValueMap::~WeakValueMap()
+    {
+        delete this->map;
+        this->map = nullptr;
+        WeakValueMap::instanceMap->erase(this->id);
+    }
 
-	void WeakValueMap::finalizationCb(const v8::WeakCallbackInfo<FinalizationCbData>& data) {
-		auto *d = data.GetParameter();
-		auto id = d->weakValueMapInstanceId;
-		// Check that WeakValueMap hasn't been deleted
-		if (WeakValueMap::instanceMap->count(id) != 0) {
-			auto *p = d->pair;
+    void WeakValueMap::finalizationCb(const v8::WeakCallbackInfo<FinalizationCbData> &data)
+    {
+        auto *d = data.GetParameter();
+        auto id = d->weakValueMapInstanceId;
+        // Check that WeakValueMap hasn't been deleted
+        if (WeakValueMap::instanceMap->count(id) != 0)
+        {
+            auto *p = d->pair;
 
-			//v8::Isolate* isolate = v8::Isolate::GetCurrent();
+            //v8::Isolate* isolate = v8::Isolate::GetCurrent();
+            auto disposer = WeakValueMap::instanceMap->at(id);
+            if (disposer)
+            {
+                disposer(p->second);
+            }
 
-			auto disposer = WeakValueMap::instanceMap->at(id);
-			if (disposer) {
-				disposer(p->second);
-			}
+            p->first->erase(p->second);
+        }
+        delete d;
+    }
 
-			//g. (p->second);// first->at(p->second).value.Get(isolate));
-			p->first->erase(p->second);
-		}
-		delete d;
-	}
+    void WeakValueMap::Set(const std::string &key, v8::Local<v8::Value> argValue)
+    {
+        v8::Isolate *isolate = v8::Isolate::GetCurrent();
 
-	void WeakValueMap::Set(const std::string &key, v8::Local<v8::Value> argValue) {
-		v8::Isolate* isolate = v8::Isolate::GetCurrent();
+        WeakValueMap *obj = this;
 
-		WeakValueMap* obj = this;// ObjectWrap::Unwrap<WeakValueMap>(args.Holder());
+        //Delete from the map if the value to insert is undefined
+        if (argValue->IsUndefined())
+            return WeakValueMap::Delete(key);
 
-		//Delete from the map if the value to insert is undefined
-		if (argValue->IsUndefined()) return WeakValueMap::Delete(key);
+        //std::string key = std::string(*v8::String::Utf8Value(args[0]->ToString()));
+        v8::UniquePersistent<v8::Value> value(isolate, argValue);
 
-		//std::string key = std::string(*v8::String::Utf8Value(args[0]->ToString()));
-		v8::UniquePersistent<v8::Value> value(isolate, argValue);
+        WeakValue val(obj->map, key, std::move(value));
+        FinalizationCbData *cbData = new FinalizationCbData(obj->id, val.pair);
+        val.value.SetWeak(cbData, WeakValueMap::finalizationCb, v8::WeakCallbackType::kParameter);
+        obj->map->insert(std::make_pair(key, std::move(val)));
 
-		WeakValue val(obj->map, key, std::move(value));
-		FinalizationCbData* cbData = new FinalizationCbData(obj->id, val.pair);
-		val.value.SetWeak(cbData, WeakValueMap::finalizationCb, v8::WeakCallbackType::kParameter);
-		obj->map->insert(std::make_pair(key, std::move(val)));
+        //args.GetReturnValue().Set(args.Holder());
+    }
 
-		//args.GetReturnValue().Set(args.Holder());
-	}
+    void WeakValueMap::Delete(const std::string &key)
+    {
+        WeakValueMap *obj = this;
+        obj->map->erase(key);
+    }
 
-	void WeakValueMap::Delete(const std::string &key) {
-		WeakValueMap* obj = this;
-		obj->map->erase(key);
-		//args.GetReturnValue().Set(args.Holder());
-	}
+    struct GetResult
+    {
+        v8::UniquePersistent<v8::Value> persistentValue;
+    };
 
+    v8::Local<v8::Value> WeakValueMap::Get(const std::string &key)
+    {
+        v8::Isolate *isolate = v8::Isolate::GetCurrent();
+        WeakValueMap *obj = this;
 
-	struct GetResult {
-		v8::UniquePersistent<v8::Value> persistentValue;
-
-	};
-
-	v8::Local<v8::Value> WeakValueMap::Get(const std::string &key) {
-		v8::Isolate* isolate = v8::Isolate::GetCurrent();
-		WeakValueMap* obj = this;
-
-		if (obj->map->count(key) == 1) {
-			return (*(obj->map))[key]
+        if (obj->map->count(key) == 1)
+        {
+            return (*(obj->map))[key]
                 .value.Get(isolate);
-		}
-		return v8::Undefined(isolate);
-	}
+        }
+        return v8::Undefined(isolate);
+    }
 
-
-	InstanceIdMap* WeakValueMap::instanceMap = new InstanceIdMap();
-	InstanceId_t WeakValueMap::instanceId = 0;
+    InstanceIdMap *WeakValueMap::instanceMap = new InstanceIdMap();
+    InstanceId_t WeakValueMap::instanceId = 0;
 }
--- WeakValueMap.h
+++ WeakValueMap.h
@@ -6,31 +6,37 @@
 //using namespace v8;
 //#include <cstdint>
 
-namespace python_node {
+namespace python_node
+{
+    struct WeakValue;
+    struct FinalizationCbData;
 
-	struct WeakValue;
-	struct FinalizationCbData;
+    typedef std::pair<
+        std::unordered_map<std::string, WeakValue> *,
+        std::string>
+        MapKeyPair;
 
-	typedef std::pair<std::unordered_map<std::string, WeakValue>*, std::string> MapKeyPair;
-	typedef std::uint64_t InstanceId_t;
-	typedef std::unordered_map<InstanceId_t, int(*)(const std::string &key)> InstanceIdMap;
+    typedef std::uint64_t InstanceId_t;
+    typedef std::unordered_map<InstanceId_t, int (*)(const std::string &key)>
+        InstanceIdMap;
 
-	class WeakValueMap {
-	public:
-		explicit WeakValueMap(int(*notifier)(const std::string &key));
-		void Set(const std::string &key, v8::Local<v8::Value> val);
-		void Delete(const std::string & key);
-		v8::Local<v8::Value> Get(const std::string &key);
+    class WeakValueMap
+    {
+    public:
+        explicit WeakValueMap(int (*notifier)(const std::string &key));
+        void Set(const std::string &key, v8::Local<v8::Value> val);
+        void Delete(const std::string &key);
+        v8::Local<v8::Value> Get(const std::string &key);
 
-	private:
-		~WeakValueMap();
-		static void finalizationCb(const v8::WeakCallbackInfo<FinalizationCbData>& data);
+    private:
+        ~WeakValueMap();
+        static void finalizationCb(const v8::WeakCallbackInfo<FinalizationCbData> &data);
 
-		static v8::Persistent<v8::Function> constructor;
-		static InstanceId_t instanceId;
-		static InstanceIdMap* instanceMap;
+        static v8::Persistent<v8::Function> constructor;
+        static InstanceId_t instanceId;
+        static InstanceIdMap *instanceMap;
 
-		InstanceId_t id;
-		std::unordered_map<std::string, WeakValue>* map;
-	};
+        InstanceId_t id;
+        std::unordered_map<std::string, WeakValue> *map;
+    };
 }
