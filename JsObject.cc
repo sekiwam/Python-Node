350759f8d91b34364812792b13ecaf8a5d6c9d66
git <git@gmail.com>
2021-04-08 04:05:01 +0900

    wo3w

--- JsObject.cc
+++ JsObject.cc
@@ -136,9 +136,12 @@ static std::unique_ptr<UiBox> _JsObject_getattr(JsObject *obj, const char *name,
 	}
 
 	if (!localValue.IsEmpty()) {
+
+        auto *uibox = new UiBox(JsValue_to_PyObject(targetLocalObject, name, localValue.ToLocalChecked()), false);
+        
         printf("[3: %s]", name);
 
-		return std::unique_ptr<UiBox>(new UiBox(JsValue_to_PyObject(targetLocalObject, name, localValue.ToLocalChecked()), false));
+		return std::unique_ptr<UiBox>(uibox);
 	}
 
 
